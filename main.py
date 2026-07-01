42f14e8008db62fdb021069f0f511499a6f1ec0b
Zaiedbabakan <zaenulhadi102@gmail.com>
2026-07-01 13:51:32 +0800

    Refactor REPLICATE_API_TOKEN assignment
    Removed unnecessary comments and os import.

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import streamlit as st
 import replicate
-import os
-
-# COLOK LANGSUNG - JANGAN PAKE SECRETS DULU
-os.environ["REPLICATE_API_TOKEN"] = "    REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]" 
-# ^^^^ GANTI ......... DENGAN SISA TOKEN KAMU SAMPE HABIS. 1 BARIS. JANGAN ENTER.
+REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]" 
